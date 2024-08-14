3029f84cc9410e7f7754dcf9b8ff2751e73c3630
Gabor Hajas <hajasgabor99@gmail.com>
2024-08-14 18:34:25 +0200

    first commit

---
+++ point_cloud_filter/point_cloud_filter.py
@@ -0,0 +1,49 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import PointCloud2
+import sensor_msgs_py.point_cloud2 as pc2
+
+class PointCloudFilter(Node):
+    def __init__(self):
+        super().__init__('point_cloud_filter')
+        self.subscription = self.create_subscription(
+            PointCloud2,
+            '/lexus3/os_center/points',
+            self.point_cloud_callback,
+            10
+        )
+        self.publisher_ = self.create_publisher(
+            PointCloud2,
+            '/lexus3/os_center/points/filtered',
+            10
+        )
+        self.z_threshold = 10.0
+
+
+    def point_cloud_callback(self, msg):
+        filtered_points = []
+        
+        for point in pc2.read_points(msg, skip_nans=True):
+            if point[2] > self.z_threshold:
+                filtered_points.append(point)
+
+        if filtered_points:
+            filtered_msg = pc2.create_cloud(
+                msg.header, msg.fields, filtered_points
+            )
+            self.publisher_.publish(filtered_msg)
+            self.get_logger().info(f'Point cloud over z = {self.z_threshold} published.')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    filter = PointCloudFilter()
+
+    rclpy.spin(filter)
+
+    filter.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
