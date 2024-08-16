42986148acbb8f498b59ee18efab1540c686c54a
Gabor Hajas <hajasgabor99@gmail.com>
2024-08-16 10:00:14 +0200

    Add parameter

--- point_cloud_filter/point_cloud_filter.py
+++ point_cloud_filter/point_cloud_filter.py
@@ -17,14 +17,17 @@ class PointCloudFilter(Node):
             '/lexus3/os_center/points/filtered',
             10
         )
-        self.z_threshold = 10.0
+        
+        self.declare_parameter('z_threshold', 1.0)
 
 
     def point_cloud_callback(self, msg):
         filtered_points = []
+
+        z_threshold = self.get_parameter('z_threshold').get_parameter_value().double_value
         
         for point in pc2.read_points(msg, skip_nans=True):
-            if point[2] > self.z_threshold:
+            if point[2] > z_threshold:
                 filtered_points.append(point)
 
         if filtered_points:
@@ -32,7 +35,7 @@ class PointCloudFilter(Node):
                 msg.header, msg.fields, filtered_points
             )
             self.publisher_.publish(filtered_msg)
-            self.get_logger().info(f'Point cloud over z = {self.z_threshold} published.')
+            self.get_logger().info(f'Point cloud over z = {z_threshold} published.')
 
 
 def main(args=None):
